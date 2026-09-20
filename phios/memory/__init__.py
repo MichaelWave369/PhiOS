525f6b038dc16f6e654fc0e07caf93bededb550e
Michael Hughes <microneesia79@gmail.com>
2026-09-20 14:05:57 -0700

    memory: export semantic retrieval adapters

--- phios/memory/__init__.py
+++ phios/memory/__init__.py
@@ -1,18 +1,35 @@
 """Governed canonical memory contracts for PhiOS."""
 
-from .models import MemoryAccessDecision, MemoryRecord, MemoryResult
+from .embeddings import EmbeddingProvider, OllamaEmbeddingProvider
+from .models import (
+    EmbeddingIdentity,
+    IndexSyncResult,
+    MemoryAccessDecision,
+    MemoryHit,
+    MemoryRecord,
+    MemoryResult,
+    VectorCandidate,
+)
 from .policy import MemoryAccessPolicy, MemoryPolicyRule
 from .service import GovernedMemoryService, RetrievalIndex, UnavailableRetrievalIndex
+from .sqlite_vec_index import SqliteVecIndex
 from .store import MemoryStore
 
 __all__ = [
+    "EmbeddingIdentity",
+    "EmbeddingProvider",
     "GovernedMemoryService",
+    "IndexSyncResult",
     "MemoryAccessDecision",
     "MemoryAccessPolicy",
+    "MemoryHit",
     "MemoryPolicyRule",
     "MemoryRecord",
     "MemoryResult",
     "MemoryStore",
+    "OllamaEmbeddingProvider",
     "RetrievalIndex",
+    "SqliteVecIndex",
     "UnavailableRetrievalIndex",
+    "VectorCandidate",
 ]
