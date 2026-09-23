5e2c57c3b5844bf9adb347b63f6940c2ebbab526
Michael Hughes <microneesia79@gmail.com>
2026-09-22 23:29:39 -0700

    fix(version): use truthful plain-string package version

--- phios/__init__.py
+++ phios/__init__.py
@@ -1,11 +1,3 @@
 """PhiOS package metadata."""
 
-
-class _CompatVersion(str):
-    def __eq__(self, other: object) -> bool:
-        if isinstance(other, str) and other == "0.3.0":
-            return True
-        return super().__eq__(other)
-
-
-__version__ = _CompatVersion("1.0.0")
+__version__ = "1.0.0"
